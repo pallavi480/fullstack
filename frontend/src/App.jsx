4a8f0cc5f82b77c0a8107a2cac6ef91fb8da18b7
Pallavi480 <pallavigayaki@gmail.com>
2026-02-23 12:24:17 +0530

    added new file

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import ProductForm from './components/ProductForm'
+import ProductList from './components/ProductList'
+
+function App() {
+
+  return (
+   <div>
+    <h1>Axios Product Crud</h1>
+    <ProductForm/>
+    <ProductList/>
+   </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/ProductForm.jsx
@@ -0,0 +1,56 @@
+import React, {useState} from 'react'
+import axios from "axios"
+
+
+const ProductForm = () => {
+    const [product,setProduct] = useState({
+        name:"",
+        price:"",
+        description:""
+    })
+  
+    const handleSubmit = async(e)=>{
+        try {
+              e.preventDefault();
+              await axios.post(
+                "http://localhost:5000/api/product/create",
+                product
+              );
+              alert("Product added")
+        } catch (error) {
+            console.log(error)
+        }
+      
+    }
+
+    const handleChange = (e)=>{
+      setProduct({...product, [e.target.name]:e.target.value})
+    }
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="name"
+        type="text"
+        placeholder="Name"
+        onChange={handleChange}
+      />
+      <input
+        name="price"
+        type="number"
+        placeholder="Price"
+        onChange={handleChange}
+      />
+      <input
+        name="description"
+        type="text"
+        placeholder="Description"
+        onChange={handleChange}
+      />
+      <button>Add Product</button>
+    </form>
+  );
+}
+
+export default ProductForm
